7cb2f6424afa8f9bddf879824f47d14b014a27ff
ask4sham <ask4sham@yahoo.co.uk>
2025-05-24 17:25:17 +0100

    ✅ Add cn() utility for Tailwind class merging

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils"; // If this causes issues, we’ll remove or rewrite it.
+
+export const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+export const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("p-4 pt-0", className)}
+    {...props}
+  />
+));
+CardContent.displayName = "CardContent";
