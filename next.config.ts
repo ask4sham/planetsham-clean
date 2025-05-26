78e25a987863f01bb228b3f10f3922858d22d487
ask4sham <ask4sham@yahoo.co.uk>
2025-05-26 17:04:27 +0100

    Enable alias @ to src in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+import path from "path";
+import type { NextConfig } from "next";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {},
   },
   webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...(config.resolve?.alias || {}),
+        "@": path.resolve(__dirname, "src"),
+      },
+    };
     return config;
   },
 };
